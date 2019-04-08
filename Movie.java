b29ed58ea3b9b513261f37ba116e9d03afc306f4
PallaviZe <49306843+PallaviZe@users.noreply.github.com>
2019-04-08 18:32:30 +0530

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,13 @@
+
+package com.company.Movie;
+public class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    void playIt(){
+        System.out.println("playing");
+    }
+
+    }
+
